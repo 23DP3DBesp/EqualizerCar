6ef5adcfd6dac8afc22bc6e352912bcaaf252b28
23DP3DBesp <A230004DB@rvt.lv>
2026-07-08 00:08:45 +0300

    Initial Commit

---
+++ EqualizerCar/EqualizerCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  EqualizerCarApp.swift
+//  EqualizerCar
+//
+//  Created by Денис Беспалов on 08/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct EqualizerCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
